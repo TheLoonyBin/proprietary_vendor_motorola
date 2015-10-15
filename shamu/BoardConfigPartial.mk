28ee77c032ba995cf9b46aed0e76586e1374de18
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-10-15 21:51:37 +0200

    Shamu: Clean slate AOSP MRA58K
    Change-Id: I341e56efa6e813943b871ebbec2be49e3b4d9fc3
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- shamu/BoardConfigPartial.mk
+++ shamu/BoardConfigPartial.mk
@@ -1,4 +1,4 @@
-# Copyright 2014 The Android Open Source Project
+# Copyright 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-LOCAL_STEM := shamu/BoardConfigPartial.mk
-
--include vendor/broadcom/$(LOCAL_STEM)
--include vendor/motorola/$(LOCAL_STEM)
--include vendor/qcom/$(LOCAL_STEM)
