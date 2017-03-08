4d0831e9b057ea31a207e5ddcb50af3b7fe8d886
gmrt <gmrt@users.noreply.github.com>
2017-03-08 23:42:12 +0100

    msm8226: Regenerate makefiles
    Change-Id: I34f4165527a4848360944ee1cc539ef08457909d
    
    Conflicts:
    
    	falcon/Android.mk
    	falcon/BoardConfigVendor.mk
    	falcon/falcon-vendor.mk
    	msm8226-common/Android.mk
    	msm8226-common/BoardConfigVendor.mk
    	msm8226-common/msm8226-common-vendor.mk
    	peregrine/Android.mk
    	peregrine/BoardConfigVendor.mk
    	peregrine/peregrine-vendor.mk
    	thea/Android.mk
    	thea/BoardConfigVendor.mk
    	thea/thea-vendor.mk
    	titan/Android.mk
    	titan/titan-vendor.mk

--- msm8226-common/Android.mk
+++ msm8226-common/Android.mk
@@ -21,6 +21,17 @@ ifneq ($(filter falcon peregrine thea titan,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.qualcomm.location
+LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location/com.qualcomm.location.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
@@ -44,17 +55,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/app/qcrilmsgtunnel/qcrilmsgtunnel.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := TimeService
-LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
